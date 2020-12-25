4076585a6bcf9df4845f825102573791c56f2a40
Tauseef Shah <tauseefsshah@gmail.com>
2020-12-25 16:05:11 +0530

    CREATED - Services page.

---
+++ resources/views/front/services.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.default')
+
+@section('beforeMainContent')
+    @include('layouts.partials.breadcrumb', ['breadcrumbPageDescription' => 'Here\'s the wide range of services that we provide'])
+@endsection
+
+@section('mainContent')
+<main>
+    <section id="about-us" class="px-6 py-20 | md:px-32 md:flex md:items-start">
+        <div class="w-full py-8 | md:py-0 md:px-8 md:w-1/3">
+            <div class="mb-8">
+                <img src="{{ asset('/assets/svg/services-web-development.svg') }}" alt="Codefuel" class="h-40 mx-auto" />
+            </div>
+            <div class="text-center">
+                <h2 class="text-2xl">
+                    Development
+                </h2>
+                <p class="text-md">
+                    Web and Mobile Apps are indeed necessary nowadays for all the businesses. We as developers understand your needs and provide custom scalable quality solutions at affordable pricing.
+                </p>
+            </div>
+        </div>
+        <div class="w-full py-8 | md:py-0 md:px-8 md:w-1/3">
+            <div class="mb-8">
+                <img src="{{ asset('/assets/svg/services-designing.svg') }}" alt="Codefuel" class="h-40 mx-auto" />
+            </div>
+            <div class="text-center">
+                <h2 class="text-2xl">
+                    Designing
+                </h2>
+                <p class="text-md">
+                    Gone are the days when good design used to be a luxury. We at Codefuel craft beautiful designs with utmost love and perfection, so you give out the good vibes to your audience.
+                </p>
+            </div>
+        </div>
+        <div class="w-full py-8 | md:py-0 md:px-8 md:w-1/3">
+            <div class="mb-8">
+                <img src="{{ asset('/assets/svg/services-marketing.svg') }}" alt="Codefuel" class="h-40 mx-auto" />
+            </div>
+            <div class="text-center">
+                <h2 class="text-2xl">
+                    Marketing
+                </h2>
+                <p class="text-md">
+                    Marketing and Strategy are important functions of a business and we are provide them both, best-in-class. Products ranging from online marketing to print media, we have it all at best pricing.
+                </p>
+            </div>
+        </div>
+    </section>
+</main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+// About
 Route::view(
     '/about',
     'front.about',
     ['title' => 'About']
-);
+)->name('front.about');
+
+// Services
+Route::view(
+    '/services',
+    'front.services',
+    ['title' => 'Services']
+)->name('front.services');
