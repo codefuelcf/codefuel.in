15e5ab6e1dceed395d6a6b242a8c46e7c2c3546a
Tauseef Shah <tauseefsshah@gmail.com>
2020-12-28 19:07:24 +0530

    IMPROVED - Navigation.

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -1,9 +1,9 @@
 <nav class=" bg-white py-4 px-6 | md:px-32 | lg:px-0 lg:py-0">
-    <ul class="list-none uppercase text-sm sm:mt-0">
-        <li class="block mr-4 ml-4 py-2 | lg:py-0 lg:ml-0 lg:inline-block"><a href="{{ route('front.home') }}">Home</a></li>
-        <li class="block mx-4 py-2 | lg:py-0 lg:inline-block"><a href="{{ route('front.about') }}">About</a></li>
-        <li class="block mx-4 py-2 | lg:py-0 lg:inline-block"><a href="#">Technologies</a></li>
-        <li class="block mx-4 py-2 | lg:py-0 lg:inline-block"><a href="{{ route('front.services') }}">Services</a></li>
-        <li class="block ml-4 py-2 | lg:py-0 lg:inline-block"><a href="#">Contact</a></li>
+    <ul class="menu list-none uppercase text-sm sm:mt-0">
+        <li class="menu-item mx-4"><a href="{{ route('front.home') }}">Home</a></li>
+        <li class="menu-item mx-4"><a href="{{ route('front.about') }}">About</a></li>
+        <li class="menu-item mx-4"><a href="#">Technologies</a></li>
+        <li class="menu-item mx-4"><a href="{{ route('front.services') }}">Services</a></li>
+        <li class="menu-item mx-4"><a href="#">Contact</a></li>
     </ul>
 </nav>
