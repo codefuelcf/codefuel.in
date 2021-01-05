e7283be30657e1ff29733f724022459d3e0bb25c
Tauseef Shah <tauseefsshah@gmail.com>
2021-01-05 15:59:12 +0530

    FIXED - Mobile navigation.

--- resources/js/front/app.js
+++ resources/js/front/app.js
@@ -3,18 +3,23 @@ import Typed from 'typed.js';
 var $ = require('jquery')
 
 $(function() {
+
     // Home hero typed
-    new Typed('#home-typed-js-hero', {
-        strings: ['Codefuel', 'Engineers', 'Developers', 'Designers',  'Creators',  'Marketers'],
-        typeSpeed: 100,
-        loop: true,
-        smartBackspace: false,
-        showCursor: false,
-    });
+    if($('#home-typed-js-hero').length)
+    {
+        new Typed('#home-typed-js-hero', {
+            strings: ['Codefuel', 'Engineers', 'Developers', 'Designers',  'Creators',  'Marketers'],
+            typeSpeed: 100,
+            loop: true,
+            smartBackspace: false,
+            showCursor: false,
+        });
+    }
 
     // Navbar icon
     $('#navbar-mobile-icon').on('click', function() {
         $('#navbar-mobile-icon').toggleClass('transform rotate-90');
         $('#navbar').toggleClass('hidden');
     });
+
 });
