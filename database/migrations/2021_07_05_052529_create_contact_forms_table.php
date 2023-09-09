eae482a8c257e17fc64c9efd24a55c41b9ea0733
Tauseef Shah <tauseefsshah@gmail.com>
2023-09-10 02:15:50 +0530

    WIP

--- database/migrations/2021_07_05_052529_create_contact_forms_table.php
+++ database/migrations/2021_07_05_052529_create_contact_forms_table.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateContactFormsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('contact_forms', function (Blueprint $table) {
             $table->id();
@@ -28,8 +28,8 @@ class CreateContactFormsTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('contact_forms');
     }
-}
+};
