b90f3ba24906399029c5800ec23197a3bc9224e3
Tauseef Shah <tauseefsshah@gmail.com>
2024-09-18 08:51:17 +0530

    UPDATE - Site favicon

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
     <meta name="robots" content="index, follow" />
     <x-front.seo-meta-tags
         :title="$title ?? 'Websites, Web Application & Designing'"
@@ -12,8 +17,6 @@
         :description="$description ?? 'Codefuel is a creative digital agency providing development, designing and hosting solutions'"
     />
 
-    <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon/favicon.ico') }}" type="image/x-icon">
-    
     <title>
         Codefuel - {{ $title ?? 'Websites, Web Application & Designing' }}
     </title>
