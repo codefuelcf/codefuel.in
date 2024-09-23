6ec8004c69babc6cd2eb3d6609a3dd959130630b
Tauseef Shah <tauseefsshah@gmail.com>
2024-09-23 18:59:30 +0530

    WIP

--- resources/js/Components/Front/Menu.jsx
+++ resources/js/Components/Front/Menu.jsx
@@ -6,9 +6,15 @@ export default function () {
   return (
     <>
       <button onClick={() => toggleMenu(!isOpen)}>Menu</button>
-      {isOpen && (
-        <div className="fixed inset-0 w-screen h-screen bg-primary text-white z-50">
-          <div className="h-20 flex justify-between items-center container">
+
+      <div
+        className={
+          "fixed inset-0 w-screen h-screen bg-primary text-white z-50 transition-opacity delay-50 duration-1000 " +
+          (isOpen ? "opacity-100 visible" : "opacity-0 invisible")
+        }
+      >
+        <div className="container">
+          <div className="h-20 flex justify-between items-center">
             <div id="navbar-logo">
               <a href="/">
                 <Logo className="h-12 text-white" />
@@ -16,8 +22,66 @@ export default function () {
             </div>
             <button onClick={() => toggleMenu(!isOpen)}>Menu</button>
           </div>
+
+          <div className="pt-20">
+            <ul className="text-4xl font-bold list-none capitalize flex flex-col gap-4 lg:text-6xl">
+              {[
+                {
+                  title: "Pricing",
+                  link: "/",
+                },
+                {
+                  title: "Contact",
+                  link: "/",
+                },
+                {
+                  title: "Privacy Policy",
+                  link: "/",
+                },
+                {
+                  title: "Return & Refunds",
+                  link: "/",
+                },
+                {
+                  title: "Terms & Conditions",
+                  link: "/",
+                },
+              ].map((el, index) => (
+                <li key={index}>
+                  <a className="text-white" href={el.link}>
+                    {el.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+
+            <ul className="text-lg font-bold flex gap-4 mt-8 list-none capitalize">
+              {[
+                {
+                  platform: "X",
+                  link: "https://x.com/codefuelcf",
+                },
+                {
+                  platform: "Github",
+                  link: "https://github.com/codefuelcf",
+                },
+              ].map((el, index) => (
+                <li key={index}>
+                  <a className="text-white" href={el.link} target="__blank">
+                    {el.platform}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="text-sm mt-8">
+            <p className="">Codefuel Software and Solutions Private Limited</p>
+            <p className="">CIN: U72900JK2021OPC012765</p>
+            <p className="">GST: 01AAJCC8036E1Z1</p>
+          </div>
         </div>
-      )}
+      </div>
     </>
   );
 }
