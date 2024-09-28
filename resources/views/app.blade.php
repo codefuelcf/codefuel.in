b324333ed23dc749e652e34a76710ac1e02cae32
Tauseef Shah <tauseefsshah@gmail.com>
2024-09-28 18:38:54 +0530

    FIX - Title to be rendered by inertia

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 
-  <title>Codefuel</title>
+  <title inertia>Codefuel</title>
 
   <!-- Fonts -->
   @googlefonts
