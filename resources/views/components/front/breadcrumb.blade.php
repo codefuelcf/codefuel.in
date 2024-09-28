83a892c05527af6c0b78558d8ff568650621be28
Tauseef Shah <tauseefsshah@gmail.com>
2024-09-28 15:09:15 +0530

    WIP

--- resources/views/components/front/breadcrumb.blade.php
+++
@@ -1,8 +0,0 @@
-<section id="breadcrumb" class="breadcrumb px-6 | md:px-32">
-    <div class="container mx-auto">
-        <p class="text-5xl text-primary">{{ $title ?? '' }}</p>
-        @if(isset($description))
-        <p class="text-xl text-white">{{ $description }}</p>
-        @endif
-    </div>
-</section>
